3e76e8392e320372a79007937c0db8646f672dd5
Kamil Kostyszyn <inkwizytor89@gmail.com>
2017-03-04 12:49:00 +0100

    Rename table teams to companies

--- patch/patch_0001-members.sql
+++ patch/patch_0001-members.sql
@@ -1,6 +1,6 @@
 \echo 'patch0001'
 
-create table if not exists teams (
+create table if not exists companies (
     id serial PRIMARY KEY,
     name varchar(40),
     fury real not null check (fury >=-1 AND fury <=1),
@@ -8,8 +8,8 @@ create table if not exists teams (
     updated TIMESTAMP default now(),
     created TIMESTAMP default now()
 );
-CREATE TRIGGER update_teams BEFORE UPDATE ON teams FOR EACH ROW EXECUTE PROCEDURE updated();
-ALTER TABLE users ADD COLUMN team_id int REFERENCES teams;
+CREATE TRIGGER update_companies BEFORE UPDATE ON companies FOR EACH ROW EXECUTE PROCEDURE updated();
+ALTER TABLE users ADD COLUMN company_id int REFERENCES companies;
 
 create table if not exists members (
     id serial PRIMARY KEY,
@@ -17,7 +17,7 @@ create table if not exists members (
     fury real not null check (fury >=-1 AND fury <=1),
     sociality real not null check (sociality >=-1 AND sociality <=1),
     sex sex_type,
-    team_id integer REFERENCES teams,
+    company_id integer REFERENCES companies,
     strength real not null default 8.0,
     agility real not null default 8.0,
     stamina real not null default 8.0,
--- unpatch/unpatch_0001.sql
+++ unpatch/unpatch_0001.sql
@@ -1,5 +1,5 @@
 \echo 'unpatch 0001'
 drop table if exists members;
 
-ALTER TABLE users DROP COLUMN team_id;
-drop table if exists teams;
+ALTER TABLE users DROP COLUMN company_id;
+drop table if exists companies;
